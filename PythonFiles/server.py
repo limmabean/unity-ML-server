979c0bd50bf50f33da79271bef9a13c1389ea8a6
Matthew Lim <limmat@gatech.edu>
2021-10-28 11:46:53 -0400

    Remove less than 100 datapoints
    added midi files

--- PythonFiles/server.py
+++ PythonFiles/server.py
@@ -3,7 +3,7 @@
 #   Binds REP socket to tcp://*:5555
 #   Expects b"Hello" from client, replies with b"World"
 #
-
+import random
 import time
 import zmq
 
@@ -11,6 +11,8 @@ context = zmq.Context()
 socket = context.socket(zmq.REP)
 socket.bind("tcp://*:5555")
 
+counter = 0
+timer = 0
 while True:
     #  Wait for next request from client
     message = socket.recv()
@@ -21,7 +23,8 @@ while True:
     #  In the real world usage, you just need to replace time.sleep() with
     #  whatever work you want python to do, maybe a machine learning task?
     time.sleep(1)
-
     #  Send reply back to client
     #  In the real world usage, after you finish your work, send your output here
-    socket.send(b"World")
+    sendMessage = str(random.randint(0, 88)) + ", " + str(random.random()) + ", " + str(timer)
+    timer = timer + 1
+    socket.send_string(sendMessage)
